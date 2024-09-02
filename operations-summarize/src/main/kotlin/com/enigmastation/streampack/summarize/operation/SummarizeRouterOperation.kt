4ea6fb5c7bce631604de7f2a450f0cc232ecfc33
Joseph Ottinger <joeo@enigmastation.com>
2024-09-02 08:37:21 -0400

    Adding longer timeout duration for summary operation

--- operations-summarize/src/main/kotlin/com/enigmastation/streampack/summarize/operation/SummarizeRouterOperation.kt
+++ operations-summarize/src/main/kotlin/com/enigmastation/streampack/summarize/operation/SummarizeRouterOperation.kt
@@ -9,12 +9,14 @@ import com.enigmastation.streampack.summarize.service.SummarizeService
 import com.enigmastation.streampack.whiteboard.model.RouterMessage
 import com.enigmastation.streampack.whiteboard.model.RouterOperation
 import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @Service
 class SummarizeRouterOperation(
     val configuration: SummarizeConfiguration,
     val summarizeService: SummarizeService
-) : RouterOperation() {
+) : RouterOperation(name="SummarizeOperation", timeout=Duration.of(2,ChronoUnit.MINUTES)) {
     override fun handleMessage(message: RouterMessage): RouterMessage? {
         if (!canHandle(message)) {
             return null
