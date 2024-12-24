365333ad4e24a8a148fe928d8ba4c478f2195d2a
Joseph Ottinger <joeo@enigmastation.com>
2024-12-24 09:38:26 -0500

    Adding rss endpoints for access to feeds

--- operations-rss/src/main/kotlin/com/enigmastation/streampack/rss/repository/RSSEntryRepository.kt
+++ operations-rss/src/main/kotlin/com/enigmastation/streampack/rss/repository/RSSEntryRepository.kt
@@ -3,6 +3,8 @@ package com.enigmastation.streampack.rss.repository
 
 import com.enigmastation.streampack.rss.entity.RSSEntry
 import com.enigmastation.streampack.rss.entity.RSSFeed
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import java.net.URL
 import java.util.Optional
 import java.util.UUID
@@ -16,4 +18,6 @@ interface RSSEntryRepository : JpaRepository<RSSEntry, UUID> {
     fun findByFeedOrderByPublishedDesc(feed: RSSFeed): List<RSSEntry>
 
     fun findRSSEntryBySummarized(summarized: Boolean): List<RSSEntry>
+
+    fun findByFeedOrderByPublishedDesc(feed: RSSFeed, page:Pageable ): Page<RSSEntry>
 }
